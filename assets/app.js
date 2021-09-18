6d8671e123b903ab15fc44b6f1468e491b183e7f
Jared Gavigan <jagavigan80@gmail.com>
2021-09-18 19:05:44 -0400

    outline of pop quis

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,20 @@
-var startButton = document.querySelector("#startbutton");
-startButton.addEventListener ("click" , function(){
 
-})
+
+
+const startButton = document.getElementById('start-btn')
+startButton.addEventListener ("click" , startGame)
+
+ function startGame() {
+     console.log('Started')
+
+ }
+
+ function setNextQuestion() {
+
+
+ }
+
+ function selectAnswer() {
+
+
+ }
