02102ca344d5a7dfc67978f7b060a848efedf66d
Jared Gavigan <jagavigan80@gmail.com>
2021-09-18 23:00:27 -0400

    almost there

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,89 @@
-
-
-
 const startButton = document.getElementById('start-btn')
-startButton.addEventListener ("click" , startGame)
+const nextbutton = document.getElementById('next-btn')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener("click" , startGame)
+nextbutton.addEventListener('click' , () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
 
  function startGame() {
-     console.log('Started')
+   startButton.classList.add('hide') 
+   shuffledQuestions = questions.sort(() => Math.random() - .5)
+   currentQuestionIndex = 0
+   questionContainerElement.classList.remove('hide') 
+   setNextQuestion()
 
  }
 
  function setNextQuestion() {
-
-
+   showQuestion(shuffledQuestions[currentQuestionIndex])
  }
 
- function selectAnswer() {
-
-
+ function showQuestion(question) {
+   questionElement.innerText = question.question
+   question.answer.forEach(answer => {
+     const button = document.createElement('button')
+     button.innertext = answer.text
+     button.classList.add('btn')
+     if (answer.correct) {
+       button.dataset.correct = answer.correct
+     }
+     button.addEventListener('click' , selectAnswer )
+     answerButtonsElement.appendChild(button)
+   })
  }
+
+ function resetState() {
+    clearStatusClass(document.body)
+    nextbutton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+      answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+ }
+
+ function selectAnswer(e) {
+   const selectedButton = e.target
+   const correct = selectedButton.dataset.correct
+   setStatusClass(document.body, correct)
+   Array.from(answerButtonElement.children).forEach(button => {
+       setStatusClass(button, button.dataset.correct)
+   })
+   if (shuffledQuestions.length > currentQuestionIndex + 1) {
+     nextbutton.classList.remove('hide')
+   } else{
+       startButton.innerText = 'restart'
+       startButton.classList.remove('hide')
+   }
+ }
+
+ function setStatusClass(element, correct) {
+   clearStatusClass(element)
+   if (correct) {
+     element.classList.add('correct')
+   } else {
+       element.classList.add('wrong')
+   }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+ 
+const questions = [
+  {
+         question: 'Commonly used data types DO Not include:',
+         answers: [
+             { text: 'strings' , correct: false },
+             { text: 'booleans' , correct: true },
+             { text: 'alerts' , correct: false },
+             {text: 'numbers' , correct: false },
+         ]
+
+     }
+    }
